9d7ec5ea776062e8cb512ff26c7bd5ff40b9ffc3
Serhii Tkachenko <doker.tk@gmail.com>
2025-01-29 15:59:34 +0100

    Initial Commit

---
+++ CoctailSwiftData/CoctailSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoctailSwiftDataApp.swift
+//  CoctailSwiftData
+//
+//  Created by Serhii on 29.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CoctailSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
